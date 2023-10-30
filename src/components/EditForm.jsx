9fa7b5ead26d4ff50875f4a3707357eff2e6a1e8
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-10-30 15:05:44 +0800

    refactored edit

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,25 +1,34 @@
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { Api } from '../Api'
+import AlertMessage from './AlertMessage'
 import { useGlobalContext } from '../hooks/useGlobalContext'
 
 export const EditForm = () => {
     const { _id } = useParams()
-    const { errors, dispatch } = useGlobalContext()
+    const { alert, errors, dispatch } = useGlobalContext()
     const [formData, setFormData] = useState({
+        //no need to over engineer like passing the item in the global state
         name: '',
         weight: '',
         size: '',
     })
     const navigate = useNavigate()
 
+    // reset states when component unmounts
+    const resetState = useCallback(() => {
+        dispatch({ type: 'SET_ITEM', payload: { item: {}, alert: {} } })
+    }, [dispatch])
+
     useEffect(() => {
+        dispatch({ type: 'CLEAR_ALERT' })
+
         const getItem = async () => {
             try {
-                const response = await axios.get(`${Api}/${_id}`);
-                const item = response.data;
+                const response = await axios.get(`${Api}/${_id}`)
+                const item = response.data
                 setFormData({
                     name: item.name,
                     weight: item.weight,
@@ -37,9 +46,11 @@ export const EditForm = () => {
                     },
                 })
             }
-        };
-        getItem();
-    },[_id, dispatch])
+        }
+
+        getItem()
+        return resetState // reset states when component unmounts
+    }, [_id, dispatch, resetState])
 
     const onChange = (e) => {
         const { name, value } = e.target
@@ -58,8 +69,8 @@ export const EditForm = () => {
         }
 
         try {
-            const response = await axios.put(`${Api}/${_id}`, item);
-            const message = response.data.success.message;
+            const response = await axios.put(`${Api}/${_id}`, item)
+            const message = response.data.success.message
             dispatch({
                 type: 'EDIT_ITEM',
                 payload: {
@@ -72,70 +83,78 @@ export const EditForm = () => {
             navigate('/')
         } catch (err) {
             const errors = err.response.data
-            dispatch({ type: 'EDIT', payload: errors })
+            dispatch({ type: 'EDIT_ITEM', payload: errors })
         }
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <div className="mb-3">
-                <label htmlFor="item-name">Name</label>
-                <input
-                    id="item-name"
-                    name="name"
-                    autoComplete="off"
-                    type="text"
-                    placeholder="Enter item name"
-                    className={`form-control ${
-                        errors && errors.name ? 'border-danger' : ''
-                    }`}
-                    onChange={onChange}
-                    value={formData.name}
-                />
-                <span className="text-danger">
-                    {errors && errors.name ? errors.name.message : ''}
-                </span>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="weight">Weight</label>
-                <input
-                    id="weight"
-                    name="weight"
-                    type="text"
-                    placeholder="Enter item weight"
-                    className={`form-control ${
-                        errors && errors.weight ? 'border-danger' : ''
-                    }`}
-                    onChange={onChange}
-                    value={formData.weight}
-                />
-                <span className="text-danger">
-                    {errors && errors.weight ? errors.weight.message : ''}
-                </span>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="size">Size</label>
-                <input
-                    id="size"
-                    name="size"
-                    type="text"
-                    placeholder="Enter item size"
-                    className={`form-control ${
-                        errors && errors.size ? 'border-danger' : ''
-                    }`}
-                    onChange={onChange}
-                    value={formData.size}
-                />
-                <span className="text-danger">
-                    {errors && errors.size ? errors.size.message : ''}
-                </span>
-            </div>
-            <div className="mb-3">
-                <button type="submit" className="btn btn-primary">
-                    Save
-                </button>
-            </div>
-        </form>
+        <div>
+            {Object.keys(alert).length === 0 ? (
+                <form onSubmit={onSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="item-name">Name</label>
+                        <input
+                            id="item-name"
+                            name="name"
+                            autoComplete="off"
+                            type="text"
+                            placeholder="Enter item name"
+                            className={`form-control ${
+                                errors && errors.name ? 'border-danger' : ''
+                            }`}
+                            onChange={onChange}
+                            value={formData.name}
+                        />
+                        <span className="text-danger">
+                            {errors && errors.name ? errors.name.message : ''}
+                        </span>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="weight">Weight</label>
+                        <input
+                            id="weight"
+                            name="weight"
+                            type="text"
+                            placeholder="Enter item weight"
+                            className={`form-control ${
+                                errors && errors.weight ? 'border-danger' : ''
+                            }`}
+                            onChange={onChange}
+                            value={formData.weight}
+                        />
+                        <span className="text-danger">
+                            {errors && errors.weight
+                                ? errors.weight.message
+                                : ''}
+                        </span>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="size">Size</label>
+                        <input
+                            id="size"
+                            name="size"
+                            type="text"
+                            placeholder="Enter item size"
+                            className={`form-control ${
+                                errors && errors.size ? 'border-danger' : ''
+                            }`}
+                            onChange={onChange}
+                            value={formData.size}
+                        />
+                        <span className="text-danger">
+                            {errors && errors.size ? errors.size.message : ''}
+                        </span>
+                    </div>
+                    <div className="mb-3">
+                        <button type="submit" className="btn btn-primary">
+                            Save
+                        </button>
+                    </div>
+                </form>
+            ) : (
+                <AlertMessage />
+            )}
+        </div>
     )
 }
 
