9d26fad12bf7688abcf1e579fb789ad69ac4557f
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-01 00:25:43 +0800

    test setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+    },
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    // css: true,
+  },
 })
